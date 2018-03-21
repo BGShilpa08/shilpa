72f45f4e0e215ae77b9b9b34bf7967ebcc04f9a6
shilpa <shilpabg97@gmail.com>
2018-03-21 10:49:38 +0530

    initial

---
+++ main.js
@@ -0,0 +1,15 @@
+function jsondata(file,callback) {
+var xhr=new XMLHttpRequest();
+xhr.overrideMimeType("application/json");
+xhr.open("GET",file,true);
+xhr.onreadystatechange=function() {
+if(xhr.readyState === 4 && xhr.status === 200){
+  callback(xhr.responseText);
+}
+};
+xhr.send();
+}
+jsondata("data.json",functon(text){
+  let data=JSON.parse(text);
+  console.log(data);
+})
